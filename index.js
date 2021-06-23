8ef90f07d0aa3661053087c0fa45d000e910b95d
mr.amirka <mr.amikra@ya.ru>
2021-06-23 17:14:15 +0300

    v0.0.2

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 #!/usr/bin/env node
 const program = require("commander");
 const pkg = require("./package.json");
-const { get } = require('mn-back-utils/request');
+const {get} = require('mn-utils/node/request');
 
 program
   .version(pkg.version, '-v, --version')
   .option("-l, --link [link]", "set Yandex.Disk public link");
 
-program.on("--help", function() {
+program.on("--help", () => {
   console.log("");
-  console.log("Examples:");
+  console.log("Example:");
   console.log("");
   console.log("$ ydlink --link https://yadi.sk/d/vZ-3Cx8mfnn_lw");
 });
 
 program.parse(process.argv);
 
-const { link } = program;
+const {link} = program;
 
 get("https://cloud-api.yandex.net:443/v1/disk/public/resources/download", {
   query: {
